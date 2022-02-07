96b85a9238109e54c90beccaad0c10597750c236
Juan David <juandavid-1527@hotmail.com>
2022-02-07 00:43:59 -0500

    an abstract class is implemented, the constructor and methods are created. The CargoShip class is extended from the Ship class.

--- src/CargoShip.java
+++ src/CargoShip.java
@@ -1,3 +1,62 @@
-public class CargoShip{
+import java.util.ArrayList;
 
+public class CargoShip extends Ship {
+    public static ArrayList<CargoShip> list = new ArrayList<>();
+    public int capacity;
+    public String solarPanel;
+
+
+    public CargoShip(String name, int weight, int power, String typeFuel, int motors, int shuttleVehicle, int capacity, String solarPanel) {
+        super(name, weight, power, typeFuel, motors, shuttleVehicle);
+        this.capacity = capacity;
+        this.solarPanel = solarPanel;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getSolarPanel() {
+        return solarPanel;
+    }
+
+    public String isSolarPanel() {
+        return solarPanel;
+    }
+
+    public void setSolarPanel(String solarPanel) {
+        this.solarPanel = solarPanel;
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public int propel() {
+        return 0;
+    }
+
+    public static CargoShip createCargoShip(String name, int weight, int power, String typeFuel, int motors, int shuttleVehicle, int capacity, String solarPanel){
+        CargoShip cargoShip = new CargoShip(name, weight, power, typeFuel, motors, shuttleVehicle, capacity,  solarPanel);
+        list.add(cargoShip);
+
+        for (int i=0; i< list.size(); i++){
+            System.out.println("name: " + list.get(i).getName());
+            System.out.println("Peso: " + list.get(i).getWeight());
+            System.out.println("Potencia: " + list.get(i).getPower());
+            System.out.println("Combustible: " + list.get(i).getTypeFuel());
+            System.out.println("Motores: " + list.get(i).getMotors());
+            System.out.println("Vehiculos de lanzadera: " + list.get(i).getShuttleVehicle());
+            System.out.println("Capacidad: " + list.get(i).getCapacity());
+            System.out.println("Panel solar: " + list.get(i).getSolarPanel());
+        }
+
+        return cargoShip;
+    }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,73 @@
+/**
+ * se crea la clase abstracta de ship
+ */
 public abstract class Ship {
+    public String name;
+    public int weight;
+    public int power;
+    public String typeFuel;
+    public int motors;
+    public int shuttleVehicle;
+
+    //constructor
+    public Ship(String name, int weight, int power, String typeFuel, int motors, int shuttleVehicle) {
+        this.name = name;
+        this.weight = weight;
+        this.power = power;
+        this.typeFuel = typeFuel;
+        this.motors = motors;
+        this.shuttleVehicle = shuttleVehicle;
+    }
+    //metodos
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getTypeFuel() {
+        return typeFuel;
+    }
+
+    public void setTypeFuel(String typeFuel) {
+        this.typeFuel = typeFuel;
+    }
+
+    public int getMotors() {
+        return motors;
+    }
+
+    public void setMotors(int motors) {
+        this.motors = motors;
+    }
+
+    public int getShuttleVehicle() {
+        return shuttleVehicle;
+    }
+
+    public void setShuttleVehicle(int shuttleVehicle) {
+        this.shuttleVehicle = shuttleVehicle;
+    }
+
+    public abstract void move();
+    public abstract int propel();
 
 }
